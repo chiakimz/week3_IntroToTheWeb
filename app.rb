235c02a29adba8045ae3d710a8c87220b85778a2
Chiaki Mizuta <chiakimizuta61@gmail.com>
2018-03-06 16:11:21 +0000

    added POST

--- app.rb
+++ app.rb
@@ -1,11 +1,11 @@
 require 'sinatra'
-get '/random-cat' do
-  @name = ["Amigo", "Oscar", "Viking"].sample	
-  erb(:index)
-end
-get '/named-cat' do
-  p params	
-  @name = params[:name]
-  erb :index	
-end	
 
+get '/cat-form' do
+  erb :cat_form
+end
+
+post '/named-cat' do
+  p params
+  @name = params[:name]
+  erb :index
+end
